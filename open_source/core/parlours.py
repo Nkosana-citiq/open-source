5c4072d4642700c905fa91f8a96917368d9d58eb
Nkosana <nkosanan@citiqprepaid.co.za>
2022-06-07 00:25:15 +0200

    Refactor daily report script to factor in manual email send for notifications.

--- open_source/core/parlours.py
+++ open_source/core/parlours.py
@@ -2,16 +2,27 @@ from random import choice, randint
 
 import hashlib
 import datetime
-
 from sqlalchemy.sql.sqltypes import Boolean
 
-from open_source import config
-from open_source import db
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from open_source import config, db, utils
+
 from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, Text, Time
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
+from open_source.core.applicants import Applicant
+from open_source.core.consultants import Consultant
+from open_source.core.payments import Payment
+from open_source.core.invoices import Invoice
+
+from open_source.core.resources import DAILY_FINANCIAL_REPORT_PER_CONSULTANT_EMAIL_TEMPLATE
+
+conf = config.get_config()
 
 
 class PasswordReset(db.Base):
@@ -88,6 +99,78 @@ class Notification(db.Base):
     def delete(self, session):
         self.make_deleted()
         session.commit()
+    
+    @staticmethod
+    def get_money_collected(cls, session, consultant):
+        applicants = session.query(Applicant).filter(Applicant.consultant_id == consultant.id, Applicant.state == Applicant.STATE_ACTIVE).all()
+        applicant_ids = [applicant.id for applicant in applicants]
+
+        payments =  session.query(Payment).filter(Payment.applicant_id.in_(applicant_ids), Payment.date >= datetime.today().date()).all()
+
+        if len(payments) > 0:
+            payment_ids = [payment.id for payment in payments]
+            invoices =  session.query(Invoice).filter(Invoice.payment_id.in_(payment_ids), Invoice.state == Invoice.STATE_ACTIVE).all()
+            return sum([invoice.amount for invoice in invoices])
+        return 0
+
+    @staticmethod
+    def send_email(cls, session, notice, parlour):
+        port = 465  # For SSL
+        smtp_server = "mail.osource.co.za"
+        sender_email = conf.SENDER_EMAIL
+        password = conf.SENDER_PASSWORD
+        to_list = [x.strip() for x in notice.recipients.split(",")]
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Daily Financial Report"
+        message["From"] = sender_email
+
+        consultants = []
+        sum = 0
+
+        for id in notice.consultants.split(", "):
+            consultant = session.query(Consultant).filter(Consultant.id == int(id), Consultant.state == Consultant.STATE_ACTIVE).first()
+            amount = cls.get_money_collected(session, consultant)
+
+            entry = """
+            <tr>
+                <td>{} {}</td>
+                <td>R{}</td>
+            </tr>""".format(consultant.first_name, consultant.last_name, amount)
+
+            consultants.append(entry)
+            sum += amount
+
+        html = {"html": """
+            {}
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>{}</strong></td>
+                <td><strong>R{}</strong></td>
+            </tr>
+            """.format(''.join(consultants), parlour.parlourname, sum)}
+
+        email_body = utils.render_template(
+            DAILY_FINANCIAL_REPORT_PER_CONSULTANT_EMAIL_TEMPLATE,
+            html
+        )
+
+        # Turn these into plain/html MIMEText objects
+        # part1 = MIMEText(text, "plain")
+        part2 = MIMEText(email_body, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        # message.attach(part1)
+        message.attach(part2)
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, to_list, message.as_string())
+
 
 
 class Parlour(db.Base):
--- open_source/rest/parlours.py
+++ open_source/rest/parlours.py
@@ -713,6 +713,9 @@ class ParlourNotificationsPostEndpoint:
             recipients = ', '.join(set(rest_dict.get('recepients')))
             consultants = ', '.join(set(rest_dict.get('consultants')))
 
+            if "all" in consultants:
+                consultants = [con.id for con in session.query(Consultant).filter(Consultant.parlour_id == parlour.id, Consultant.state == Consultant.STATE_ACTIVE).all()]
+
             notify_times = rest_dict.get('time').split(":")
             notify_time = time(int(notify_times[0]), int(notify_times[1]))
 
@@ -730,3 +733,64 @@ class ParlourNotificationsPostEndpoint:
             notification.save(session)
 
             resp.body = json.dumps([], default=str)
+
+
+class ParlourNotificationsSendEmailEndpoint:
+
+    def __init__(self, secure=False, basic_secure=False):
+        self.secure = secure
+        self.basic_secure = basic_secure
+
+    def is_basic_secure(self):
+        return self.basic_secure
+
+    def is_not_secure(self):
+        return not self.secure
+
+    def on_post(self, req, resp, id):
+
+        with db.transaction() as session:
+            rest_dict = json.load(req.bounded_stream)
+
+            if not rest_dict.get('recepients'):
+                raise falcon.HTTPBadRequest(title="Error", description="Users are required.")
+
+            if not rest_dict.get('days'):
+                raise falcon.HTTPBadRequest(title="Error", description="Days of the week are required.")
+
+            if not rest_dict.get('time'):
+                raise falcon.HTTPBadRequest(title="Error", description="Time to send notification not set.")
+
+            if not rest_dict.get('consultants'):
+                raise falcon.HTTPBadRequest(title="Error", description="Select at least one conusltant or select the parlour.")
+
+            parlour = session.query(Parlour).filter(
+                Parlour.id == id,
+                Parlour.state == Parlour.STATE_ACTIVE).first()
+
+            if not parlour:
+                raise falcon.HTTPBadRequest("Parlour does not exist.")
+
+            days = ', '.join(set(rest_dict.get('days')))
+            recipients = ', '.join(set(rest_dict.get('recepients')))
+            consultants = ', '.join(set(rest_dict.get('consultants')))
+
+            if "all" in consultants:
+                consultants = [con.id for con in session.query(Consultant).filter(Consultant.parlour_id == parlour.id, Consultant.state == Consultant.STATE_ACTIVE).all()]
+
+            notify_times = rest_dict.get('time').split(":")
+            notify_time = time(int(notify_times[0]), int(notify_times[1]))
+
+            notification = Notification(
+                recipients = recipients,
+                week_days = days,
+                scheduled_time = notify_time,
+                parlour_id = parlour.id,
+                consultants = consultants,
+                state = Notification.STATE_ACTIVE,
+                modified_at = datetime.now(),
+                created_at = datetime.now()
+            )
+
+            Notification.send_email(session, notification, parlour)
+            resp.body = json.dumps({"status": "Success"}, default=str)
